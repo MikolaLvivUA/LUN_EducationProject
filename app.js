322c51a6b6b998982469c12469917fd8d48eb6c0
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2019-10-24 13:41:20 +0300

    Socket.io + support chat

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express'); //require express
+const expHbs = require('express-handlebars');
 const path = require('path'); //require path for working with different OC path
 const fileUpload = require('express-fileupload'); //require  fileUpload
 
@@ -6,10 +7,37 @@ const app = express(); //create our server
 const dataBase = require('./dataBase').getInstance();
 dataBase.setModels();
 
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+
+    socket.on('joinRoom', data => {
+        socket.join(data.room_id);
+
+        console.log(data);
+    });
+
+    socket.on('msgToRoom', data => {
+        console.log(data);
+        io.to(data.room_id).emit('sendMsgToRoom', {id: data.socket_id, data: data.data})
+    })
+});
+
 app.use(fileUpload()); //use our fileUpload for all our Routes
 app.use(express.json()); //teach our express read JSON files
 app.use(express.urlencoded({extended: true})); //teach our express parse JSON files;
 app.use(express.static(path.join(__dirname, 'static'))); // teach our express works with static directory
+
+app.engine('.hbs', expHbs({ // setting our template engine
+    extname: '.hbs',
+    defaultLayout: null //important thing!!!
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'static'));
+
 global.appRoot = __dirname; //Our main directory.
 
 //MODULES
@@ -19,10 +47,14 @@ const {usersRouter, housesRouter, authRouter} = require('./router');
 app.use('/users', usersRouter);
 app.use('/houses', housesRouter);
 app.use('/auth', authRouter);
+//SupportPage
+app.get('/support', (req, res) => {
+    res.render('support')
+});
 //404
 app.all('*', render404.render404Page);
 
-app.listen(5000, () => { //create our server port-listener
+http.listen(5000, () => { //create our server port-listener
     console.log('listen port: 5000');
     console.log(
         '██╗  ██╗███████╗██╗     ██╗      ██████╗     ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗  ██╗ █████╗ ██╗   ██╗███████╗    ███████╗████████╗ █████╗ ██████╗ ████████╗███████╗██████╗     ██╗     ██╗   ██╗███╗   ██╗\n' +
